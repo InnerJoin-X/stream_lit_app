5ca96a34c72560db992e31092a17523e6e13fc39
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-07-24 19:21:53 +0530

    Update streamlit_app.py
    multiselect filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import pandas
 import streamlit
 
+
 my_fruit_list = pandas.read_csv(' https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocada','Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocada","Strawberries"])
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
