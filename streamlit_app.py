3270bec6c45540ecf37d018edf95158d4e76a181
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-07-24 19:02:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 my_fruit_list = pandas.read_csv(' https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streeamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacoda','Strawberries'])
+streeamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocada','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
