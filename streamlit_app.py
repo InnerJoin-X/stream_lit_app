3c8f29afe3434e02bb5558979dd7360e0f7aa542
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-07-24 18:23:43 +0530

    Update streamlit_app.py
    testing_1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My parents are out for dinner')
-streamlit.write('Breakfast Menu')
-streamlit.write('Crosaint & Coffee')
-streamlit.write('Coffee & Crosaint')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
