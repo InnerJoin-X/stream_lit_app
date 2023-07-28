cb507452881d2435f7843892292b7451625c517c
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-07-28 12:27:53 +0530

    Update streamlit_app.py
    dataframe for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ import pandas as pd
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
