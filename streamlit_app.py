13cf30fc6212d2c52d07a1929d3524684b20c3a2
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-07-28 12:34:52 +0530

    Update streamlit_app.py
    fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,14 @@ import streamlit
 import requests
 import pandas as pd
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# streamlit.text(fruityvice_response.json())
 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+streamlit.header('Fruityvice fruit advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+fruityvice_response = response.get(""https://fruityvice.com/api/fruit"+ fruit_choice)
+
+# fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# streamlit.dataframe(fruityvice_normalized)
