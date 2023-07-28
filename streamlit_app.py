52fef3e460835bd7c206f3791d319f1962fddf44
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-07-28 12:25:52 +0530

    Update streamlit_app.py
    fruity_response.json()

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
+import streamlit
 import requests
+import pandas as pd
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
