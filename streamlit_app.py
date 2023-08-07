3727820a0ca40880bfa3b08debab4eb89bc85580
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-08-07 17:19:23 +0530

    Update streamlit_app.py
    retest

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,9 @@
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+import streamlit 
 
-# import streamlit
-# import requests
-# import pandas as pd
+streamlit.title('My parents New Healthy Dinner')
 
-# # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# # streamlit.text(fruityvice_response.json())
-
-# streamlit.header('Fruityvice fruit advice!')
-# fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-# streamlit.write('The user entered', fruit_choice)
-
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
-
-# # fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# # streamlit.dataframe(fruityvice_normalized)
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
