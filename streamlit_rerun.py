d30fcb27b9f89d99b00df029ad846909e1ca6fc2
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-08-07 17:22:54 +0530

    Create streamlit_rerun.py
    rerun for badge 2

---
+++ streamlit_rerun.py
@@ -0,0 +1,8 @@
+import streamlit 
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Test')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
