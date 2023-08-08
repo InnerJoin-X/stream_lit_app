218d132e108e28b57cbce91bfd070f21607d822f
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-08-08 18:20:36 +0530

    Update streamlit_app.py
    removed all from streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,1 @@
-import streamlit 
 
-streamlit.title('My parents New Healthy Dinner')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
