b881bb8517500576508935a1353b64f927d46ff3
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-08-09 15:22:15 +0530

    Update streamlit_app.py
    indentation fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,9 @@ streamlit.dataframe(fruits_to_show)
 #     else:
 
 def get_fruityvice_data(this_fruit_choice):
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit', fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-return fruityvice_normalized
+    fruityvice_response = requests.get('https://fruityvice.com/api/fruit', fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
 streamlit.header('Fruityvice Fruit Advice!')
 try:
