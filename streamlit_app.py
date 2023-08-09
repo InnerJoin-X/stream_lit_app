4d7c443c03e46c7af41faad12bee86cca6ea579b
InnerJoin-X <97040241+InnerJoin-X@users.noreply.github.com>
2023-08-09 14:22:55 +0530

    Update streamlit_app.py
    streamlit stop added

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
